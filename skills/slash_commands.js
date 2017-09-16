cb2f17af32d497c9e56ec36b58879ebafdd1ef3d
Schuyler Duveen <sky@skyb.us>
2017-09-16 13:16:30 -0400

    add auto team saving

--- skills/slash_commands.js
+++ skills/slash_commands.js
@@ -2,7 +2,8 @@ module.exports = function(controller) {
 
   controller.on('slash_command', function(bot, message) {
     bot.replyPublic(message, 'foooo my app')
-    console.log('SLASH COMMAND', message)
+    console.log('SLASH COMMAND BOT', bot)
+    console.log('SLASH COMMAND MESSAGE', message)
   })
 
 
