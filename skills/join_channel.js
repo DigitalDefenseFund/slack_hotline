ca6b92e5a672228edfd6bba419afc1aff13eee61
Angelina Bethoney <ambethoney@yahoo.com>
2017-09-16 18:58:59 -0400

    Incl join leave funct

--- skills/join_channel.js
+++ skills/join_channel.js
@@ -1,5 +1,4 @@
 module.exports= function(controller){
-  console.log("this is being called on join");
   controller.on('user_channel_join',function(bot,message) {
     console.log("here usr: " + message.user)
   });
--- skills/leave_channel.js
+++ skills/leave_channel.js
@@ -1,5 +1,4 @@
 module.exports= function(controller){
-  console.log("this is being called on leave");
   controller.on('user_channel_join',function(bot,message) {
     console.log("here usr: " + message.user)
   });
