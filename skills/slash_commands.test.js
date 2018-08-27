9bc937536e3f10dbe250ddc9404c6b4da08f4fa0
BintLopez <nmlopez21@gmail.com>
2018-08-27 12:41:19 -0500

    comment

--- skills/slash_commands.test.js
+++ skills/slash_commands.test.js
@@ -3,3 +3,5 @@ const slash_commands = require('./slash_commands');
 test('adds 1 to the argument', () => {
   expect(slash_commands(1)).toBe(2);
 });
+
+// https://jestjs.io/docs/en/expect#tohavebeencalled
