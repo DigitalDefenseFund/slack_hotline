047d0df04b81b85df5483cab876db7b841a77b21
BintLopez <nmlopez21@gmail.com>
2018-10-09 17:03:11 -0500

    add test for channel create

--- skills/channel_create.js
+++ skills/channel_create.js
@@ -5,9 +5,10 @@ module.exports= function(controller){
       'team_id': message.team_id
     }
     controller.storage.channels.save(channel, function(err, savedChannel) {
-      if (!err) {
-        console.log('Channel saved successfully')
-      } else {
+      if (err) {
+        // Do we want to throw an error here?
+        // Or do we want to log an error here?
+        // Should set up some error logging like Sentry
         console.log(`Channel ${message.channel_id} not persisted to data.`)
       }
     })
